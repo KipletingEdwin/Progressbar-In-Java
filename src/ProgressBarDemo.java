93a515e3fa889a15e773529c633ec8d16058d26d
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-08-15 15:25:50 +0300

    add setDefaultCloseOperation to the frame

--- src/ProgressBarDemo.java
+++ src/ProgressBarDemo.java
@@ -3,4 +3,13 @@ import javax.swing.*;
 public class ProgressBarDemo {
 
     JFrame frame = new JFrame();
+    JProgressBar bar = new JProgressBar();
+
+    ProgressBarDemo(){
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+    }
+
+
 }
